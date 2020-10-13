415457725dad04e5393e686017593bd68b0ab895
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-10-13 19:43:48 -0300

    fix(back): add field openin_hours in create_orphanages migration

--- backend/src/database/migrations/1602607228409-create_orphanages.ts
+++ backend/src/database/migrations/1602607228409-create_orphanages.ts
@@ -41,6 +41,10 @@ export class createOrphanages1602607228409 implements MigrationInterface {
             type: 'text',
           },
           {
+            name: 'opening_hours',
+            type: 'varchar',
+          },
+          {
             name: 'open_on_weekends',
             type: 'boolean',
             default: false,
