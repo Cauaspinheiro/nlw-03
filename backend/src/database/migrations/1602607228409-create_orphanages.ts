62c778fd43b749ef9d9c5c219be4c6d5c05be9df
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-10-13 15:02:05 -0300

    feat(back): add migration for table orphanages

---
+++ backend/src/database/migrations/1602607228409-create_orphanages.ts
@@ -0,0 +1,56 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+const TABLE_NAME = 'orphanages'
+
+export class createOrphanages1602607228409 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: TABLE_NAME,
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'latitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'longitude',
+            type: 'decimal',
+            scale: 10,
+            precision: 2,
+          },
+          {
+            name: 'about',
+            type: 'text',
+          },
+          {
+            name: 'instructions',
+            type: 'text',
+          },
+          {
+            name: 'open_on_weekends',
+            type: 'boolean',
+            default: false,
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(TABLE_NAME)
+  }
+}
