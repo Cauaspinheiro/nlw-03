8a5b312ed6f6e7544157fedc54b2c09b446901d1
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-10-13 22:36:20 -0300

    feat(back): add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,9 +3,11 @@ import 'express-async-errors'
 import routes from './routes'
 import './database/connection'
 import errorHandler from './errors/handler'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use(routes)
