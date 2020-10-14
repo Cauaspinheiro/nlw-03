da2b148abcae7978fadecf43073d65fb34bf0693
Cauaspinheiro <56241309+Cauaspinheiro@users.noreply.github.com>
2020-10-13 21:33:53 -0300

    feat(back): add route to return static files in folder upload

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express'
+import express, { Router } from 'express'
 import { ListOrphanagesRoute } from './features/ListOrphanages'
 import { CreateOrphanageRoute } from './features/CreateOrphanage'
 import { ShowOrphanageRoute } from './features/ShowOrphanage'
+import path from 'path'
 
 const routes = Router()
 
 routes.use(CreateOrphanageRoute)
 routes.use(ListOrphanagesRoute)
 routes.use(ShowOrphanageRoute)
+routes.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 export default routes
